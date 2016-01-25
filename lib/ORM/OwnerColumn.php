b5f8c38de2ba78796eb05908ee308c2d73b569ce
Mark Armendariz <src@enobrev.com>
2016-01-25 07:26:58 -0600

    added getOwnerField to owenrColumn Interface

--- lib/ORM/OwnerColumn.php
+++ lib/ORM/OwnerColumn.php
@@ -2,6 +2,8 @@
     namespace Enobrev\ORM;
 
     interface OwnerColumn {
+        public function getOwnerField();
+
         public function getOwner();
 
         public function hasOwner(Table $oOwner);

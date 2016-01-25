c4c8f155e25b5462ee8e46a83a4f0640c6ebf759
Mark Armendariz <src@enobrev.com>
2016-01-25 07:30:23 -0600

    added some docs for typing owner column tables

--- lib/ORM/OwnerColumn.php
+++ lib/ORM/OwnerColumn.php
@@ -2,9 +2,19 @@
     namespace Enobrev\ORM;
 
     interface OwnerColumn {
+        /**
+         * @return Field
+         */
         public function getOwnerField();
 
+        /**
+         * @return Table
+         */
         public function getOwner();
 
+        /**
+         * @param Table $oOwner
+         * @return bool
+         */
         public function hasOwner(Table $oOwner);
     }

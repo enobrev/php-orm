f9642ed82496626946103041a8c9e363981a40c8
Mark Armendariz <src@enobrev.com>
2016-02-22 08:07:03 -0600

    added ches to depluralize method

--- util/sql_to_json.php
+++ util/sql_to_json.php
@@ -451,13 +451,14 @@
 
     function depluralize($word){
         $rules = array(
-            'ss'  => false,
-            'os'  => 'o',
-            'ies' => 'y',
-            'xes' => 'x',
-            'oes' => 'o',
-            'ves' => 'f',
-            's'   => ''
+            'ss'   => false,
+            'os'   => 'o',
+            'ies'  => 'y',
+            'xes'  => 'x',
+            'oes'  => 'o',
+            'ves'  => 'f',
+            'ches' => 'ch',
+            's'    => ''
         );
 
         foreach(array_keys($rules) as $key){

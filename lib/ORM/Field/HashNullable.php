1b256788141f8883966b901ce8133b6dce506aea
Mark Armendariz <src@enobrev.com>
2016-08-17 19:37:05 -0500

    Added Nullable-specific methods to Nullable fields

--- lib/ORM/Field/HashNullable.php
+++ lib/ORM/Field/HashNullable.php
@@ -1,7 +1,53 @@
 <?php
     namespace Enobrev\ORM\Field;
 
+    use Enobrev\ORM\Table;
+
     class HashNullable extends Hash {
+        /**
+         *
+         * @return string|NULL
+         */
+        public function getValue() {
+            $sValue = $this->sValue;
+
+            if (strlen(trim($sValue)) == 0) {
+                $sValue = NULL;
+            }
+
+            if (strtolower($sValue) == 'null') {
+                $sValue = NULL;
+            }
+
+            return $sValue;
+        }
+
+        /**
+         *
+         * @param mixed $sValue
+         * @return HashNullable
+         */
+        public function setValue($sValue) {
+            if ($sValue instanceof Table) {
+                $sValue = $sValue->{$this->sColumn};
+            }
+
+            if ($sValue instanceof Field) {
+                $sValue = $sValue->getValue();
+            }
+
+            if (strlen(trim($sValue)) == 0) {
+                $sValue = NULL;
+            }
+
+            if (strtolower($sValue) == 'null') {
+                $sValue = NULL;
+            }
+
+            $this->sValue = $sValue;
+
+            return $this;
+        }
 
         /**
          *
--- lib/ORM/Field/UUIDNullable.php
+++ lib/ORM/Field/UUIDNullable.php
@@ -1,7 +1,53 @@
 <?php
     namespace Enobrev\ORM\Field;
 
+    use Enobrev\ORM\Table;
+
     class UUIDNullable extends UUID {
+        /**
+         *
+         * @return string|NULL
+         */
+        public function getValue() {
+            $sValue = $this->sValue;
+
+            if (strlen(trim($sValue)) == 0) {
+                $sValue = NULL;
+            }
+
+            if (strtolower($sValue) == 'null') {
+                $sValue = NULL;
+            }
+
+            return $sValue;
+        }
+
+        /**
+         *
+         * @param mixed $sValue
+         * @return UUIDNullable
+         */
+        public function setValue($sValue) {
+            if ($sValue instanceof Table) {
+                $sValue = $sValue->{$this->sColumn};
+            }
+
+            if ($sValue instanceof Field) {
+                $sValue = $sValue->getValue();
+            }
+
+            if (strlen(trim($sValue)) == 0) {
+                $sValue = NULL;
+            }
+
+            if (strtolower($sValue) == 'null') {
+                $sValue = NULL;
+            }
+
+            $this->sValue = $sValue;
+
+            return $this;
+        }
 
         /**
          *

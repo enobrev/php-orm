4718cd88e0e7ded9f8e5de8b213badf293ffb775
Mark Armendariz <src@enobrev.com>
2017-06-09 03:08:34 -0500

    having another go at adjusting the phpdocs for Tables classes

--- lib/ORM/Tables.php
+++ lib/ORM/Tables.php
@@ -19,7 +19,7 @@
         }
 
         /**
-         * @return Table[]
+         * @return Table[]|Tables
          */
         public static function get() {
             $oTable   = static::getTable();
@@ -45,7 +45,7 @@
          * @param Table $oTable
          * @param array $aData
          * @param array $aMap
-         * @return Table[]
+         * @return Table[]|Tables
          */
         public static function createAndUpdateFromMap(Table $oTable, Array $aData, Array $aMap) {
             $aOutput = new static;
@@ -61,7 +61,7 @@
         /**
          * @param Table $oTable
          * @param array $aData
-         * @return Table[]
+         * @return Table[]|Tables
          */
         public static function createAndUpdate(Table $oTable, Array $aData) {
             $aOutput = new static;
@@ -77,7 +77,7 @@
         /**
          * @param PDOStatement $oResults
          * @param Table[] ...$aTables
-         * @return Table[]
+         * @return Table[]|Tables
          */
         protected static function fromResults(PDOStatement $oResults, ...$aTables) {
             if (count($aTables) > 1) {

a70c7474d55611c3058d937fcb1c5701355ef1fe
Mark Armendariz <src@enobrev.com>
2017-12-19 04:15:12 -0600

    Fixed php-doc on OwnerColumn interface:

--- lib/ORM/OwnerColumn.php
+++ lib/ORM/OwnerColumn.php
@@ -3,7 +3,7 @@
 
     interface OwnerColumn {
         /**
-         * @return static
+         * @return Table
          */
         public function getOwnerTable();
 
@@ -13,7 +13,7 @@
         public function getOwnerField();
 
         /**
-         * @return static
+         * @return Table
          */
         public function getOwner();
 

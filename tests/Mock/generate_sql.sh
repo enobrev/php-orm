b9855bb831217cc13f8736105f072f60497e6977
Mark Armendariz <src@enobrev.com>
2019-10-21 21:14:47 -0500

    Updated mysql2sqlite script

--- tests/Mock/generate_sql.sh
+++ tests/Mock/generate_sql.sh
@@ -6,20 +6,21 @@ HOST=192.168.1.2
 ../../bin/sql_to_json.php -h ${HOST} -u dev -p -n dev_orm_mock
 
 # generate mysql.sql
-mysqldump -h ${HOST} -u dev -p --no-data --databases dev_orm_mock > mysql.sql
+mysqldump -h ${HOST} -u dev -p --no-data --skip-extended-insert --compact --skip-quote-names --databases dev_orm_mock > mysql.sql
 
-# download script for converting mysql database to sqlite
+# download script for converting mysql database to sqlite - https://github.com/dumblob/mysql2sqlite/blob/master/mysql2sqlite
 if [ ! -f ./mysql2sqlite.sh ]; then
-    wget https://gist.githubusercontent.com/esperlu/943776/raw/be469f0a0ab8962350f3c5ebe8459218b915f817/mysql2sqlite.sh
+    wget https://raw.githubusercontent.com/dumblob/mysql2sqlite/master/mysql2sqlite
+    mv mysql2sqlite mysql2sqlite.sh
     chmod +x mysql2sqlite.sh
 fi
 
 # generate sqlite.sql
-./mysql2sqlite.sh -h ${HOST} -u dev -p --no-data --databases dev_orm_mock > sqlite.sql
+./mysql2sqlite.sh mysql.sql > sqlite.sql
 sed -i '/CREATE DATABASE/d' ./sqlite.sql
 sed -i '/CONSTRAINT/d' ./sqlite.sql
 sed -i '/PRAGMA/d' ./sqlite.sql
 sed -i '/TRANSACTION/d' ./sqlite.sql
 sed -i '/CREATE INDEX/d' ./sqlite.sql
-sed -i 's/"address_id" int(10)  NOT NULL/"address_id" INTEGER/' ./sqlite.sql
-sed -i 's/"user_id" int(10)  NOT NULL/"user_id" INTEGER/' ./sqlite.sql
+sed -i 's/address_id int(10)  NOT NULL/address_id INTEGER/' ./sqlite.sql
+sed -i 's/user_id int(10)  NOT NULL/user_id INTEGER/' ./sqlite.sql
--- tests/Mock/mysql.sql
+++ tests/Mock/mysql.sql
@@ -1,72 +1,29 @@
--- MySQL dump 10.13  Distrib 5.7.26, for Linux (x86_64)
---
--- Host: 192.168.1.2    Database: dev_orm_mock
--- ------------------------------------------------------
--- Server version	5.7.25
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Current Database: `dev_orm_mock`
---
 
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `dev_orm_mock` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
-USE `dev_orm_mock`;
-
---
--- Table structure for table `addresses`
---
-
-DROP TABLE IF EXISTS `addresses`;
+USE dev_orm_mock;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `addresses` (
-  `address_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned DEFAULT NULL,
-  `address_line_1` varchar(100) DEFAULT NULL,
-  `address_city` varchar(50) DEFAULT NULL,
-  `address_date_added` datetime DEFAULT NULL,
-  `address_date_updated` datetime DEFAULT NULL,
-  PRIMARY KEY (`address_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `addresses_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE addresses (
+  address_id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  user_id int(10) unsigned DEFAULT NULL,
+  address_line_1 varchar(100) DEFAULT NULL,
+  address_city varchar(50) DEFAULT NULL,
+  address_date_added datetime DEFAULT NULL,
+  address_date_updated datetime DEFAULT NULL,
+  PRIMARY KEY (address_id),
+  KEY user_id (user_id),
+  CONSTRAINT addresses_user_id FOREIGN KEY (user_id) REFERENCES `users` (user_id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(50) DEFAULT NULL,
-  `user_email` varchar(100) DEFAULT NULL,
-  `user_happy` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `user_date_added` datetime DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
+CREATE TABLE users (
+  user_id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  user_name varchar(50) DEFAULT NULL,
+  user_email varchar(100) DEFAULT NULL,
+  user_happy tinyint(1) unsigned NOT NULL DEFAULT '0',
+  user_date_added datetime DEFAULT NULL,
+  PRIMARY KEY (user_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-05-03 18:49:46

9d41243f9ebd15cf6586ea34b8174f58e58c49a2
Mark Armendariz <src@enobrev.com>
2020-02-20 14:09:22 -0600

    Reverting last change because it makes no difference.  MySQL will auto-increment for a 0-value

--- lib/ORM/Field/Id.php
+++ lib/ORM/Field/Id.php
@@ -2,4 +2,10 @@
     namespace Enobrev\ORM\Field;
 
     class Id extends Integer {
+        /**
+         * @return bool
+         */
+        public function hasValue():bool {
+            return parent::hasValue() && (int) $this->sValue > 0;
+        }
     }

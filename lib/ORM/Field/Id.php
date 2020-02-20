3cbb4e623a325b3abea321f0b749c01d50df34cf
Mark Armendariz <src@enobrev.com>
2020-02-20 14:03:04 -0600

    Field\Id::hasValue no longer checks for > 0 as 0 is a valid value

--- lib/ORM/Field/Id.php
+++ lib/ORM/Field/Id.php
@@ -2,10 +2,4 @@
     namespace Enobrev\ORM\Field;
 
     class Id extends Integer {
-        /**
-         * @return bool
-         */
-        public function hasValue():bool {
-            return parent::hasValue() && (int) $this->sValue > 0;
-        }
     }

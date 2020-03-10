5227bc0e6d90ca72cdf22dae39bebcf6e84ec5c2
Mark Armendariz <src@enobrev.com>
2020-03-10 13:05:17 -0500

    Reverting last change

--- lib/ORM/Field/JSONText.php
+++ lib/ORM/Field/JSONText.php
@@ -2,17 +2,4 @@
     namespace Enobrev\ORM\Field;
 
     class JSONText extends TextNullable {
-
-        /**
-         *
-         * @param mixed $sValue
-         * @return $this
-         */
-        public function setValue($sValue) {
-            if (is_array($sValue) || is_object($sValue)) {
-                $sValue = json_encode($sValue);
-            }
-
-            return parent::setValue($sValue);
-        }
     }

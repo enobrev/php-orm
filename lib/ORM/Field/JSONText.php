ec9825d0924fb0d03549d40066ce590fb02679aa
Mark Armendariz <src@enobrev.com>
2020-03-10 13:23:06 -0500

    var check

--- lib/ORM/Field/JSONText.php
+++ lib/ORM/Field/JSONText.php
@@ -1,5 +1,36 @@
 <?php
     namespace Enobrev\ORM\Field;
 
+    use Enobrev\ORM\Field;
+    use Enobrev\ORM\Table;
+
     class JSONText extends TextNullable {
+
+        /**
+         * Same as TextNullable but does a string check before trying to trim
+         *
+         * @param mixed $sValue
+         * @return $this
+         */
+        public function setValue($sValue) {
+            if ($sValue instanceof Table) {
+                $sValue = $sValue->{$this->sColumn};
+            }
+
+            if ($sValue instanceof Field) {
+                $sValue = $sValue->getValue();
+            }
+
+            if (is_string($sValue) && empty(trim($sValue))) {
+                $sValue = NULL;
+            }
+
+            if ($sValue === 'null' || $sValue === 'NULL') {
+                $sValue = NULL;
+            }
+
+            $this->sValue = $sValue;
+
+            return $this;
+        }
     }

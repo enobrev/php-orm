f4eb55f34de38db6a228c26b205bd52658ba4273
Mark Armendariz <src@enobrev.com>
2020-03-10 12:49:35 -0500

    json_encode JSONText fields in cases where setValue is setting an array or object

--- lib/ORM/Field/JSONText.php
+++ lib/ORM/Field/JSONText.php
@@ -2,4 +2,17 @@
     namespace Enobrev\ORM\Field;
 
     class JSONText extends TextNullable {
+
+        /**
+         *
+         * @param mixed $sValue
+         * @return $this
+         */
+        public function setValue($sValue) {
+            if (is_array($sValue) || is_object($sValue)) {
+                $sValue = json_encode($sValue);
+            }
+
+            return parent::setValue($sValue);
+        }
     }

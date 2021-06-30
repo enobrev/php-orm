5c29e73c374cd4182e15ce6cf219cee7b6ad72b8
Mark Armendariz <src@enobrev.com>
2021-06-30 13:24:01 -0500

    Fixed database collation in Db

--- lib/ORM/Db.php
+++ lib/ORM/Db.php
@@ -107,7 +107,7 @@
             $oPDO->setAttribute(PDO::ATTR_ERRMODE,            PDO::ERRMODE_EXCEPTION);
             $oPDO->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             $oPDO->setAttribute(PDO::MYSQL_ATTR_FOUND_ROWS,   true);
-            $oPDO->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci');
+            $oPDO->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, 'SET NAMES utf8mb4 COLLATE utf8mb4_general_ci');
 
             return $oPDO;
         }
